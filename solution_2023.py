29f8271a1c2eb774c4b4f5757e7ba74deacdb317
Trevor Lee <52921992+Lee-88@users.noreply.github.com>
2023-12-02 14:42:23 +0000

    Update solution_2023.py

--- solution_2023.py
+++ solution_2023.py
@@ -18,6 +18,26 @@ for li in f:
     total += int(num1+num2)
 f.close()
 print(total)
+#--------------Part 2
+import re
+f = open("ELFLIST.txt", "r")
+total = 0
+for li in f:
+    elfdata = li.replace("one", "o1e")
+    elfdata = elfdata.replace("two", "t2o")
+    elfdata = elfdata.replace("three", "t3e")
+    elfdata = elfdata.replace("four", "f4r")
+    elfdata = elfdata.replace("five", "f5e")
+    elfdata = elfdata.replace("six", "s6x")
+    elfdata = elfdata.replace("seven", "s7n")
+    elfdata = elfdata.replace("eight", "e8t")
+    elfdata = elfdata.replace("nine", "n9e")
+    elfdata = re.sub("[^1-9]", "", elfdata)
+    num1 = elfdata[0]
+    num2 = elfdata[-1]
+    total += int(num1+num2)
+f.close()
+print(total)
 ##--------------------------------------------
 #DAY 2
 
